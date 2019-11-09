a2bd380313e719f5655234e2bafaf69d4e3ac5c8
Ugwuododo Jude Obinna <jobinna50@gmail.com>
2019-11-10 00:08:12 +0100

    ft: create get-feed-for-article-and-gifs endpoint[#34]

--- routes/employee.js
+++ routes/employee.js
@@ -10,5 +10,6 @@ router.delete('/articles/:id', auth, employeeCtrl.deleteArticle);
 router.delete('/gifs/:id', auth, employeeCtrl.deleteGif);
 router.post('/articles/:id/comment', auth, employeeCtrl.createCommentArticle);
 router.post('/gifs/:id/comment', auth, employeeCtrl.gifCreateComment);
+router.get('/feed', employeeCtrl.newsFeed);
 
 module.exports = router;
