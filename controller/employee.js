bc4c7311f008a8d1cea0243880e38ddf12e66fc6
Ugwuododo Jude Obinna <jobinna50@gmail.com>
2019-11-13 10:21:35 +0100

    new app

--- controller/employee.js
+++ controller/employee.js
@@ -3,28 +3,23 @@ const pool = require('../services/dbconfig');
 const cloudinary = require('../services/cloudinary_config');
 
 exports.createGifs = (req, res) => {
-  const { title, image } = req.body;
+  const { title } = req.body;
+  const image = req.file[0].path;
   const createdOn = new Date();
   const token = req.headers.authorization.split(' ')[1];
   const decodedToken = jwt.verify(token, 'AM-HAPPY');
   const { userId } = decodedToken;
-  cloudinary.upload(image)
-  .then((result) => {
-    const gifdetails = [
-      result.url,
-      result.public_id
-    ];
-    
-    pool.query('INSERT INTO gifstb(title, imageUrl, createdOn, authorid)VALUES($1,$2,$3,$4) RETURNING gifId As id', [title, gifdetails, createdOn, userId])
-    .then((data) => {
+  cloudinary.upload(image).then((gif) => {
+    const imageUrl = gif.secure_url;
+    pool.query('INSERT INTO gifstb(title, imageUrl, createdOn, authorid)VALUES($1,$2,$3,$4) RETURNING gifId As id', [title, imageUrl, createdOn, userId]).then((results) => {
       res.status(201).json({
         status: "success",
         data: {
-          gifId: data.rows[0].id,
+          gifId: results.rows[0].id,
           message: "GIF image successfully posted",
           createdOn,
           title,
-          gifdetails
+          imageUrl
         }
       });
     }).catch(() => {
