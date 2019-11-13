63314114ba88c196c7782f31de39a5dc3ee09584
Ugwuododo Jude Obinna <jobinna50@gmail.com>
2019-11-13 11:22:50 +0100

    new app

--- controller/employee.js
+++ controller/employee.js
@@ -3,8 +3,7 @@ const pool = require('../services/dbconfig');
 const cloudinary = require('../services/cloudinary_config');
 
 exports.createGifs = (req, res) => {
-  const image = req.file.path;
-  const { title } = req.body;
+  const { title, image } = req.body;
   const createdOn = new Date();
   const token = req.headers.authorization.split(' ')[1];
   const decodedToken = jwt.verify(token, 'AM-HAPPY');
