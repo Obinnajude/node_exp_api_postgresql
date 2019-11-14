bd895c6c9c54e0bb1d19b3abbb1b0331062ceeeb
Ugwuododo Jude Obinna <jobinna50@gmail.com>
2019-11-14 21:36:36 +0100

    new app

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ app.use('/api/v2/', adminRoute);
 app.use('/api/v2/', signInRoute);
 app.use('/api/v2/', employeeRoutes);
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '/index.html'));
+});
+app.get('/api/v2', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '/index.html'));
 });
 module.exports = app;
